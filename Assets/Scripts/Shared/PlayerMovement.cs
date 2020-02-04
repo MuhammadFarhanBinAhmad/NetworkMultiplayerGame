284471b14e22c86b4968faf51f2aa277df73216d
CheekyKnob <44768125+MuhammadFarhanBinAhmad@users.noreply.github.com>
2020-02-04 22:34:03 +0800

    Anim change

--- Assets/Scripts/Shared/PlayerMovement.cs
+++ Assets/Scripts/Shared/PlayerMovement.cs
@@ -5,19 +5,21 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    public enum CurrentCharacter
+    {
+        Survivor,
+        Killer
+    }
+    public CurrentCharacter the_Current_Character;
     //---Player Stats
     //Player Movement and Physic
     [Header("Player Stats")]
     public float starting_Movement_Speed;
     public float move_Speed;
 
-    public Image health_Bar;
-
     Animator player_Anim;
 
     //Player Movement
-    public bool currently_Moving;
     CharacterController theCC;
     Vector3 moveDirection;
 
@@ -27,9 +29,46 @@ public class PlayerMovement : MonoBehaviour
         theCC = GetComponent<CharacterController>();
         player_Anim = GetComponent<Animator>();
         move_Speed = starting_Movement_Speed;
+
+        //for killer
+        if (player_Anim.layerCount == 2)
+        {
+            player_Anim.SetLayerWeight(1, .5f);
+        }
     }
 
     void FixedUpdate()
+    {
+        /*//PlayerMovement
+        float Ystore = moveDirection.y;//store whatever y direction player currently in
+
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        moveDirection = (transform.forward * vertical * move_Speed) + (transform.right * horizontal * move_Speed);//move in correlation with the directin of the camera & enable player to strafe
+        moveDirection = moveDirection.normalized * move_Speed;
+
+        theCC.Move(moveDirection * Time.deltaTime);//enable player to move in the world
+
+        player_Anim.SetFloat("Running", vertical);
+        player_Anim.SetFloat("Strafing", horizontal);
+
+
+        if (GetComponent<PlayerManager>().is_Hurt)
+        {
+            player_Anim.SetBool("Injured", true);
+            move_Speed = starting_Movement_Speed / 2;
+        }
+        else
+
+        {
+            player_Anim.SetBool("Injured", false);
+            move_Speed = starting_Movement_Speed;
+        }*/
+        PlayerMovementControl();
+
+    }
+    void PlayerMovementControl()
     {
         //PlayerMovement
         float Ystore = moveDirection.y;//store whatever y direction player currently in
@@ -45,6 +84,42 @@ public class PlayerMovement : MonoBehaviour
         player_Anim.SetFloat("Running", vertical);
         player_Anim.SetFloat("Strafing", horizontal);
 
+        switch (the_Current_Character)
+        {
+            case CurrentCharacter.Survivor:
+                if (GetComponent<PlayerManager>().is_Hurt)
+                {
+                    player_Anim.SetBool("Injured", true);
+                    move_Speed = starting_Movement_Speed / 2;
+                }
+                else
 
+                {
+                    player_Anim.SetBool("Injured", false);
+                    move_Speed = starting_Movement_Speed;
+                }
+                break;
+            case CurrentCharacter.Killer:
+
+                AnimatorStateInfo current_Base_State;
+                AnimatorStateInfo attack_State;
+
+                current_Base_State = player_Anim.GetCurrentAnimatorStateInfo(0);
+
+                if (player_Anim.layerCount == 2)
+                {
+                    attack_State = player_Anim.GetCurrentAnimatorStateInfo(1);
+                }
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    player_Anim.SetBool("Attacking", true);
+                }
+                else
+                {
+                    player_Anim.SetBool("Attacking", false);
+                }
+                break;
+        }
     }
 }
